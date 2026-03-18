07bb06fd76a6cd86027b1fd67d335e231319c78c
QUANTUM-YASH <sonamsomkar57@gmail.com>
2026-03-18 20:37:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 import cloudinary
 import cloudinary.uploader
 import hashlib
+import os
 
 app = Flask(__name__)
 
@@ -82,4 +83,5 @@ def admin():
 
 # ------------------ RUN ------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT automatically
+    app.run(host="0.0.0.0", port=port)
